908f883c58817aa5f0659f49110c15ac1aeb466c
bluejoyq <bluejoy32@gmail.com>
2023-04-19 00:22:06 +0900

    feat: global 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@ import { router } from "@router";
 import { type ReactElement } from "react";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Global, css } from "@emotion/react";
+import reset from "emotion-reset";
 export function App(): ReactElement {
   return (
     <QueryClientProvider client={new QueryClient()}>
+      <Global
+        styles={css`
+          ${reset}
+          *, *::before, *::after {
+            box-sizing: border-box;
+          }
+        `}
+      />
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
