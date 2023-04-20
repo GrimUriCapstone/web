2c7757eef894b76e01118ae1669a578936c1ff4c
bluejoyq <bluejoy32@gmail.com>
2023-04-20 21:44:48 +0900

    refactor: 회원가입 페이지에서 tailwind 걷어냄

--- src/presentation/signUpPage/components/SignUpForm.tsx
+++ src/presentation/signUpPage/components/SignUpForm.tsx
@@ -2,10 +2,20 @@ import { useUserRepository } from "@data/repository/userRepository";
 import { notificationStore } from "@data/stores/notificationStore";
 import { useUserStore } from "@data/stores/userStore";
 import { HOME_PAGE_PATH } from "@domain/constants/paths";
+import { type SerializedStyles } from "@emotion/react";
+
+import {
+  Button,
+  TextField,
+  css,
+  Checkbox,
+  useTheme,
+  type Theme,
+} from "@mui/material";
 import { LoadingModal } from "@presentation/common/components/LoadingModal";
 import { useMutation } from "@tanstack/react-query";
 import { type ReactElement, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function SignUpForm(): ReactElement {
   const [nickname, setNickname] = useState("");
@@ -27,25 +37,76 @@ export function SignUpForm(): ReactElement {
   const handleSubmit = (): void => {
     mutate({ nickname });
   };
-
+  const theme = useTheme();
   return (
     <>
       {isLoading && <LoadingModal />}
-      <div className="form-control w-full max-w-xs">
-        <label className="label">
-          <span className="label-text">Nickname</span>
-        </label>
-        <input
-          type="text"
-          placeholder="Type here"
-          maxLength={20}
-          className="input input-bordered w-full max-w-xs"
-          onChange={(e) => {
-            setNickname(e.target.value);
-          }}
-        />
+      <TextField
+        id="outlined-basic"
+        label="Nickname"
+        variant="outlined"
+        css={css`
+          width: 100%;
+          max-width: 300px;
+        `}
+        inputProps={{ maxLength: 20 }}
+        onChange={(e) => {
+          setNickname(e.target.value);
+        }}
+      />
+      <div
+        css={css`
+          width: 100%;
+          max-width: 300px;
+          font-size: 13px;
+          display: flex;
+          flex-direction: column;
+          align-items: end;
+        `}
+      >
+        <div>
+          <Checkbox />
+          <label>
+            <Link
+              to={
+                "https://meadow-locust-124.notion.site/b6efb378d4ab485a8f7c11551b8ced13"
+              }
+              css={linkStyles(theme)}
+            >
+              이용 약관
+            </Link>
+            에 동의합니다.
+          </label>
+        </div>
+        <div>
+          <Checkbox />
+          <label>
+            <Link
+              to={
+                "https://meadow-locust-124.notion.site/196322b07bc24a0db86ad9ee74d3ee66"
+              }
+              css={linkStyles(theme)}
+            >
+              개인정보이용방침
+            </Link>
+            에 동의합니다.
+          </label>
+        </div>
       </div>
-      <input type="submit" onClick={handleSubmit} />
+      <Button
+        variant="contained"
+        onClick={handleSubmit}
+        css={css`
+          width: 100%;
+          max-width: 300px;
+        `}
+      >
+        회원가입하기
+      </Button>
     </>
   );
 }
+
+const linkStyles = (theme: Theme): SerializedStyles => css`
+  color: ${theme.palette.primary.main};
+`;
