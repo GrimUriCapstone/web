56ec1d233513546be0ce2bfe44bf37dead54f368
bluejoyq <bluejoy32@gmail.com>
2023-06-01 22:51:07 +0900

    fix: user가 null이면 못보게

--- src/presentation/mainPage/MainPage.tsx
+++ src/presentation/mainPage/MainPage.tsx
@@ -9,8 +9,13 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { DiaryPreview } from "@presentation/common/components/DiaryPreview";
 import { DiaryPreviewSkeleton } from "@presentation/common/components/DiaryPreview.skeleton";
 import { BottomScrollListener } from "react-bottom-scroll-listener";
+import { useUserStore } from "@data/stores/userStore";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import { SETTINGS_PAGE_PATH } from "@domain/constants/paths";
 export function MainPage(): ReactElement {
   const { getRecentDiaries } = useDirayRepository();
+  const { user } = useUserStore();
   const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
     useInfiniteQuery({
       queryKey: ["diary", "getRecentDiary"],
@@ -29,7 +34,12 @@ export function MainPage(): ReactElement {
         <MainTitle />
         <BottomScrollListener
           onBottom={() => {
-            if (hasNextPage === false || hasNextPage === undefined) return;
+            if (
+              hasNextPage === false ||
+              hasNextPage === undefined ||
+              user == null
+            )
+              return;
             fetchNextPage();
           }}
         >
@@ -39,19 +49,30 @@ export function MainPage(): ReactElement {
               flex-direction: column;
               gap: 16px;
               width: 100%;
+              justify-content: center;
+              align-items: center;
             `}
           >
-            {isLoading || data == null
-              ? [1, 2, 3].map((_) => <DiaryPreviewSkeleton key={_} />)
-              : data.pages
-                  .map((page) => page.content)
-                  .flat()
-                  .map((diary) => (
-                    <DiaryPreview diary={diary} key={diary.diaryId} />
-                  ))}
+            {user == null && (
+              <Link to={SETTINGS_PAGE_PATH}>
+                <Button variant="contained">로그인해주세요</Button>
+              </Link>
+            )}
+            {user != null && (
+              <>
+                {isLoading || data == null
+                  ? [1, 2, 3].map((_) => <DiaryPreviewSkeleton key={_} />)
+                  : data.pages
+                      .map((page) => page.content)
+                      .flat()
+                      .map((diary) => (
+                        <DiaryPreview diary={diary} key={diary.diaryId} />
+                      ))}
 
-            {isFetchingNextPage &&
-              [1, 2, 3].map((_) => <DiaryPreviewSkeleton key={_} />)}
+                {isFetchingNextPage &&
+                  [1, 2, 3].map((_) => <DiaryPreviewSkeleton key={_} />)}
+              </>
+            )}
           </div>
         </BottomScrollListener>
         <div
